066479dcafb3ec196ec0e4f9be8dd13fc7ada844
lqCi-sun <le.quang.chien@sun-asterisk.com>
2024-07-24 16:56:32 +0700

    Chapter_3_4_5

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module RailsTutorial
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :vi]
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+      get 'demo_partials/new'
+    get 'demo_partials/edit'
+    get 'static_pages/home'
+    get 'static_pages/help'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    root "static_pages#home"
+  end
 end
