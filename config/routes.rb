1bf2d73665f19aed01935a1ba6e08814c2abadbe
lqCi-sun <le.quang.chien@sun-asterisk.com>
2024-07-31 16:51:54 +0700

    Chapter_12

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
     get "static_pages/home"
     get "static_pages/help"
-
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     resources :users, only: %i(new create show edit update index destroy)
@@ -12,7 +11,7 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
-
+    resources :password_resets, only: %i(new create edit update)
     resources :account_activations, only: :edit
   end
 end
